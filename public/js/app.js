34ecaa1871756a8573e717498b676457a8a5840d
Michael Hadjiosif <michael.hadjiosif@feisystems.com>
2016-05-18 02:58:53 -0400

    Move Hello component to new Hello.js file and import Hello component into app.js

--- public/js/app.js
+++ public/js/app.js
@@ -1,16 +1,6 @@
 import React from "react";
 import ReactDOM from"react-dom";
 
-// let Hello = React.createClass({
-	// render() {
-		// return <h3>Hello ES6</h3>;
-	// }
-// });
-
-class Hello extends React.Component {
-	render() {
-		return <h3>Hello React with ES6</h3>;
-	}
-}
+import Hello from "./components/Hello";
 
 ReactDOM.render(<Hello />, document.getElementById('react'));

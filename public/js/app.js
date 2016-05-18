8d059060dc446ea37cc66c04cb5417508e075a08
Michael Hadjiosif <michael.hadjiosif@feisystems.com>
2016-05-18 02:52:01 -0400

    Use ES6 class syntax instead of varable to define React component in app.js

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,16 @@
 let React = require("react");
 let ReactDOM = require("react-dom");
 
-let Hello = React.createClass({
+// let Hello = React.createClass({
+	// render() {
+		// return <h3>Hello ES6</h3>;
+	// }
+// });
+
+class Hello extends React.Component {
 	render() {
-		return <h3>Hello ES6</h3>;
+		return <h3>Hello React with ES6</h3>;
 	}
-});
+}
 
 ReactDOM.render(<Hello />, document.getElementById('react'));

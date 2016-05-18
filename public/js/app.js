5f422be9bbd58b9594f11893f8f8eb95f8137a78
Michael Hadjiosif <michael.hadjiosif@feisystems.com>
2016-05-18 02:44:17 -0400

    Modify app.js to use JSX syntax

--- public/js/app.js
+++ public/js/app.js
@@ -3,8 +3,8 @@ var ReactDOM = require("react-dom");
 
 var Hello = React.createClass({
 	render: function() {
-		return React.createElement("h3", null, "Hello Webpack!");
+		return <h3>Hello JSX</h3>;
 	}
 });
 
-ReactDOM.render(React.createElement(Hello), document.getElementById('react'));
+ReactDOM.render(<Hello />, document.getElementById('react'));

2ab503df039c14d86b571aa00b6397e8fd289886
Michael Hadjiosif <michael.hadjiosif@feisystems.com>
2016-05-20 12:02:43 -0400

    Add incrementCounter mutation to schema.js

--- data/schema.js
+++ data/schema.js
@@ -5,17 +5,29 @@ import {
 	GraphQLString
 } from 'graphql';
 
+let counter = 42;
+
 let schema = new GraphQLSchema({
 	query: new GraphQLObjectType({
 		name: 'Query',
 		fields: () => ({
 			counter: {
 				type: GraphQLInt,
-				resolve: () => 42
+				resolve: () => counter
 			},
 			message: {
 				type: GraphQLString,
 				resolve: () => "Hello GraphQL!"
+			}
+		})
+	}),
+	
+	mutation: new GraphQLObjectType({
+		name: 'Mutation',
+		fields: () => ({
+			incrementCounter: {
+				type: GraphQLInt,
+				resolve: () => ++counter
 			}
 		})
 	})

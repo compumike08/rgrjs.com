e6d61d6ca9c8e189696202a9d9570c0ed3478ff4
Michael Hadjiosif <michael.hadjiosif@feisystems.com>
2016-05-23 19:52:34 -0400

    Add React Optimistic Update support

--- js/components/Link.js
+++ js/components/Link.js
@@ -8,7 +8,13 @@ class Link extends React.Component {
 		fontSize: '0.7em',
 		marginRight: '0.5em'
 	})
-	dateLabel = () => moment(this.props.link.createdAt).format('L')
+	dateLabel = () => {
+		let {link, relay} = this.props;
+		if (relay.hasOptimisticUpdate(link)) {
+			return 'Saving...';
+		}
+		return moment(link.createdAt).format('L');
+	}
 	render() {
 		let {link} = this.props;
 		return (

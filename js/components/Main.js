9d6c241797447409d88e162022a0f39a3cd44702
Michael Hadjiosif <michael.hadjiosif@feisystems.com>
2016-05-22 20:40:16 -0400

    Modify GraphQL schema to have 'store' field as root node containing links, modify Main.js and app.js to use GraphQL fragment, modify app.js to use Relay Route

--- js/components/Main.js
+++ js/components/Main.js
@@ -1,10 +1,5 @@
 import React from "react";
-import API from "../API";
-import LinkStore from "../stores/LinkStore";
-
-let _getAppState = () => {
-	return { links: LinkStore.getAll() };
-};
+import Relay from "react-relay";
 
 class Main extends React.Component {
 	static propTypes = {
@@ -15,20 +10,8 @@ class Main extends React.Component {
 		limit: 4
 	}
 	
-	state = _getAppState();
-	
-	componentDidMount(){
-		API.fetchLinks();
-		LinkStore.on("change", this.onChange);
-	}
-	componentWillUnmount(){
-		LinkStore.removeListener("change", this.onChange);
-	}
-	onChange = () => {
-		this.setState(_getAppState());
-	}
 	render() {
-		let content = this.state.links.slice(0, this.props.limit).map(link => {
+		let content = this.props.store.links.slice(0, this.props.limit).map(link => {
 		return <li key={link._id}>
 					<a href={link.url}>{link.title}</a>
 				</li>
@@ -43,5 +26,19 @@ class Main extends React.Component {
 		);
 	}
 }
+
+Main = Relay.createContainer(Main, {
+	fragments: {
+		store: () => Relay.QL`
+			fragment on Store {
+				links {
+					_id,
+					title,
+					url,
+				}
+			}
+		`
+	}
+});
 
 export default Main;
